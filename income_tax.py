aeb4ca5983e8e0828c210890b50b2b51c3598fe6
unknown <you@example.com>
2024-01-26 13:57:54 +0800

    pseudocode

---
+++ income_tax.py
@@ -0,0 +1,15 @@
+# pseudocode
+
+# Create a function to calculate tax
+
+    # If income <= 10,000
+
+    # If income <= 20,000
+
+    # else
+
+# Ask for income
+
+# Create variable to call the function
+
+# Print Income and Tax
