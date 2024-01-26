a79f3f6115dbe8849eb6b8a033205c3475403664
unknown <you@example.com>
2024-01-26 14:01:40 +0800

    function done

--- income_tax.py
+++ income_tax.py
@@ -1,12 +1,18 @@
 # pseudocode
 
 # Create a function to calculate tax
-
+def income_tax(income):
     # If income <= 10,000
-
+    if income <= 10000:
+        payable_tax = 0
     # If income <= 20,000
-
+    elif income <= 20000:
+        payable_tax = (income - 10000) * 0.10
     # else
+    else:
+        payable_tax = 10000 * 0.10 + (income - 20000) * 0.20
+   
+    return payable_tax
 
 # Ask for income
 
